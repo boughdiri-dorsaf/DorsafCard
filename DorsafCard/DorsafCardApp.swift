32f637219744eca8b3654e3ed4e1cac1e11ddbde
boughdiri-dorsaf <boughdiridorsaf12@gmail.com>
2022-12-23 11:54:22 +0100

    Initial Commit

---
+++ DorsafCard/DorsafCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DorsafCardApp.swift
+//  DorsafCard
+//
+//  Created by Boughdiri Dorsaf on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DorsafCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
